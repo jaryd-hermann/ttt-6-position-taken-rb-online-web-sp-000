ded25469551ab18d282ad898d6118afd5576c192
Jaryd Hermann <hermannjaryd@gmail.com>
2021-01-27 12:41:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == "X"
-
-  elseif board[index] == "O"
-
+  elsif board[index] == "O"
   else (board[index] == " ") || (board[index] == "")
   end
 end
